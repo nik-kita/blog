9aa00769076918d2d7ba7cc13590251ecf19a3fd
nik <volsknik@gmail.com>
2020-10-04 19:43:18 +0300

    YES! MIDDLEWARE 'MYAUTH' NOW IS WORKING CORRECTLY!
    When user try to perform actions that are availeble only in authorized
    mode, but he is not in it --
    this middleware will redirect him to the page where
    for him will propouse to make authorization or registration.
    
    THE MAIN FEATURE in this process is button 'Back'.
    How it works?
    If user don't follow advice and click button back. It will
    redirect him whenever he come from. Except no site urls and
    currently this middleware url. In these cases he will redirected
    to home page.

--- app/Http/Middleware/MyAuth.php
+++ app/Http/Middleware/MyAuth.php
@@ -11,18 +11,25 @@ class MyAuth
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Auth::check()) {
+        if (Auth::check()) {
             return $next($request);
-        } else {
-            $back = $request->session()->get('url')['intended'];
+        }
+        if($request->session()->previousUrl() != null) {
+            $back = $request->session()->previousUrl();
+            if($request->session()->previousUrl() == $request->url()) {
+                $back = route('home.index');
+            }
             return response(view('propouseauth')
                 ->with('back', $back));
+        } else {
+            return redirect()->route('home.index');
         }
+
     }
 }
