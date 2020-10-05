083c4a5dbbc040873613ddd1af6d9477f47a89ab
nik <volsknik@gmail.com>
2020-10-05 10:27:06 +0300

    CREATE Post model and first migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,10 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
+    public function posts() {
+        return $this->hasMany('App\Models\Post');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
