62ba4783312fcd28ee00a8e668c1d3914856cda3
nik <volsknik@gmail.com>
2020-10-07 08:40:22 +0300

    FIX Comment dsplay 'none' or 'block'

--- resources/views/show-post.blade.php
+++ resources/views/show-post.blade.php
@@ -13,14 +13,16 @@
     <a href="{{ route('addComment', ['id' => $post->id]) }}">
         <button>Add Comment</button>
     </a>
-    <button onclick="document.getElementById('comments').style.visibility='visible'">Show Comments</button>
-    <button onclick="document.getElementById('comments').style.visibility='hidden'">Hide Comments</button>
-    <div id="comments" style="visibility: hidden">
-        @foreach($post->comments()->get() as $c)
+    @if(count($post->comments()->get()) > 0)
+        <button onclick="document.getElementById('comments').style.display='block'">Show Comments</button>
+        <button onclick="document.getElementById('comments').style.display='none'">Hide Comments</button>
+        <div id="comments" style="display: none">
+            @foreach($post->comments()->get() as $c)
 
-            <x-comment text="{{ $c->body }}" author="{{ App\Models\User::find($c->user_id)->name }}"/>
-        @endforeach
-    </div>
+                <x-comment text="{{ $c->body }}" author="{{ App\Models\User::find($c->user_id)->name }}"/>
+            @endforeach
+        </div>
+    @endif
 
 
 
